b2ad6f7085a31c769712a502e95a9db9c2611f0b
Carmen Janse van Rensburg <carmen@bbd.co.za>
2022-10-05 10:20:50 +0200

    audio in lobby

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,65 +1,3 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class SoundManager : MonoBehaviour
-//{
-//    private static SoundManager instance = null;
-//    public static SoundManager Instance
-//    {
-//        get { return instance; }
-//    }
-
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-
-//    }
-
-//    //void Awake()
-//    //{
-//    //    if(instance != null && instance != this)
-//    //    {
-//    //        Destroy(this.gameObject);
-//    //        return;
-//    //    }
-//    //    else
-//    //    {
-//    //        instance = this;
-//    //    }
-
-//    //    DontDestroyOnLoad(this.gameObject)
-//    //}
-
-
-//    void Awake()
-//    {
-//        if (instance == null)
-//        {
-//            Instance = this;
-//            DontDestroyOnLoad(gameObject);
-//        }
-//        else
-//        {
-//            Destroy(gameObject)
-//        }
-//    }
-
-//    public void PlaySound(AudioClip clip)
-//    {
-
-//    }
-
-//}
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -86,6 +24,13 @@ public class SoundManager : MonoBehaviour
 
     public void PlaySound(AudioClip clip)
     {
+        _effectsSource.PlayOneShot(clip);
+    }
+
+
+    public void PlayShatter(AudioClip clip)
+    {
+        AudioListener.volume = 0.08f;
         _effectsSource.PlayOneShot(clip);
     }
 

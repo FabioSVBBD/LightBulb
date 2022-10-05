e49699d96be5c38fa3e923eabba35957ff0c43cb
Carmen Janse van Rensburg <carmen@bbd.co.za>
2022-10-05 10:26:47 +0200

    less hardcoded camera start position

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
+    private float originalCameraX ;
+
     void Start()
     {
+        originalCameraX = Camera.main.transform.position.x; 
     }
 
     void Update()
     {
-        if (player.transform.position.x > -1.762109)
+        if (player.transform.position.x > originalCameraX)
         {
             transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
         }
